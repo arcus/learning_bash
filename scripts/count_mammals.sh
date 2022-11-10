2230d92fdf9ff8276b2d632baa109dfcd68cecee
DRELLICHE <drelliche@chop.edu>
2022-11-10 15:08:40 -0500

    Update count_mammals.sh

--- scripts/count_mammals.sh
+++ scripts/count_mammals.sh
@@ -1,8 +1,6 @@
 # This script returns a count of how many animals in Animals.csv are mammals.
 # This script shoule be run from the directory containing the animal_name.txt files.
 
-
-
 count=`grep mammal Animals.csv | wc -l`
 
 echo There are $count mammals on the list.
